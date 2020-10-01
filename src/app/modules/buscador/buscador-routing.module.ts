10a469e4442cfd61e9b61d2cd809f2717ca8147c
hugoh92 <hugoholiveira45@gmail.com>
2020-10-01 15:18:42 -0300

    estilizacao tabela buscador curso

--- src/app/modules/buscador/buscador-routing.module.ts
+++ src/app/modules/buscador/buscador-routing.module.ts
@@ -1,7 +1,8 @@
 import { Routes } from "@angular/router";
+import { from } from 'rxjs';
 import { BuscaComponent } from 'src/app/header/buscador/busca/busca.component';
 import { PesquisaAvancadaComponent } from 'src/app/header/buscador/pesquisa-avancada/pesquisa-avancada.component';
-
+import {ResultadoComponent} from 'src/app/header/buscador/resultado/resultado.component'
 export const BuscadorRoutes: Routes = [
   {
     path: 'buscador',
@@ -10,5 +11,10 @@ export const BuscadorRoutes: Routes = [
   {
     path: 'buscador/avancado',
     component: PesquisaAvancadaComponent,
+  },
+  {
+    path: 'buscador/resultado',
+    component: ResultadoComponent,
   }
+
 ]
