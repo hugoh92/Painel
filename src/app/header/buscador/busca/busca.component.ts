eee47b61dce6d6de43e3f5d3522f044205fbf18b
hugoh92 <hugoholiveira45@gmail.com>
2020-11-30 17:53:26 -0300

    Abas resultados

--- src/app/header/buscador/busca/busca.component.ts
+++ src/app/header/buscador/busca/busca.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import * as Highcharts from 'highcharts';
 import { DataService } from 'src/app/services/data.service';
 
 
@@ -24,6 +25,7 @@ export class BuscaComponent implements OnInit {
   } 
 
   ngOnInit(): void {
+    // this.testeGraf();
   }
 
   textEntered(e){
@@ -33,5 +35,39 @@ export class BuscaComponent implements OnInit {
   valueSelected(e){
     this.selectedObject = JSON.stringify(e);
   }
+  // testeGraf(){
+  //   Highcharts.chart('otonome', {
+
+  //     title: {
+  //         text: 'Logarithmic axis demo'
+  //     },
+  
+  //     xAxis: {
+  //         tickInterval: 1,
+  //         type: 'logarithmic',
+  //         accessibility: {
+  //             rangeDescription: 'Range: 1 to 10'
+  //         }
+  //     },
+  
+  //     yAxis: {
+  //         type: 'logarithmic',
+  //         minorTickInterval: 0.1,
+  //         accessibility: {
+  //             rangeDescription: 'Range: 0.1 to 1000'
+  //         }
+  //     },
+  
+  //     tooltip: {
+  //         headerFormat: '<b>{series.name}</b><br />',
+  //         pointFormat: 'x = {point.x}, y = {point.y}'
+  //     },
+  
+  //     series: [{
+  //         data: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
+  //         pointStart: 1
+  //     }]
+  // });
+  // }
 
 }
