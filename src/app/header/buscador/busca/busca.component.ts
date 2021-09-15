c88db9b57f80d921e168b1e3ae0b7692953cd9a6
Gabi <gabriela.borges@neoway.com.br>
2021-09-15 16:10:55 -0300

    fix autocomplete

--- src/app/header/buscador/busca/busca.component.ts
+++ src/app/header/buscador/busca/busca.component.ts
@@ -25,7 +25,6 @@ export class BuscaComponent implements OnInit {
   } 
 
   ngOnInit(): void {
-    // this.testeGraf();
   }
 
   textEntered(e){
@@ -35,39 +34,4 @@ export class BuscaComponent implements OnInit {
   valueSelected(e){
     this.selectedObject = JSON.stringify(e);
   }
-  // testeGraf(){
-  //   Highcharts.chart('otonome', {
-
-  //     title: {
-  //         text: 'Logarithmic axis demo'
-  //     },
-  
-  //     xAxis: {
-  //         tickInterval: 1,
-  //         type: 'logarithmic',
-  //         accessibility: {
-  //             rangeDescription: 'Range: 1 to 10'
-  //         }
-  //     },
-  
-  //     yAxis: {
-  //         type: 'logarithmic',
-  //         minorTickInterval: 0.1,
-  //         accessibility: {
-  //             rangeDescription: 'Range: 0.1 to 1000'
-  //         }
-  //     },
-  
-  //     tooltip: {
-  //         headerFormat: '<b>{series.name}</b><br />',
-  //         pointFormat: 'x = {point.x}, y = {point.y}'
-  //     },
-  
-  //     series: [{
-  //         data: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
-  //         pointStart: 1
-  //     }]
-  // });
-  // }
-
 }
