22ed0d119ec8ddf4211d3ae45bf0d909feae526d
Gabi <gabriela.borges@neoway.com.br>
2021-11-04 21:28:25 -0300

    adicionar funcionamento do backed nos botões

--- src/app/header/data-explorer/data-explorer.component.ts
+++ src/app/header/data-explorer/data-explorer.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'YYYY',
+  },
+  display: {
+    dateInput: 'YYYY',
+    monthYearLabel: 'YYYY',
+    monthYearA11yLabel: 'YYYY',
+  },
+};
 
 @Component({
   selector: 'app-data-explorer',
@@ -14,10 +21,34 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
   
 export class DataExplorerComponent implements OnInit {
-  
-  constructor() { }
+  metricaSelecionada = "qt_vaga_total";
+  cruzamentoSelecionado = "cat_admin";
+
+  currentCheckedValue = null;
+  constructor(private ren: Renderer2) { } 
 
   ngOnInit(): void {
+  }
+
+  chosenYearHandler(ev, input){
+    let { _d } = ev;
+    input._destroyPopup()
+  }
+
+
+
+  checkState(el) {
+    setTimeout(() => {
+      console.log(el)
+      if (this.currentCheckedValue && this.currentCheckedValue === el.value) {
+        el.checked = false;
+        this.ren.removeClass(el['_elementRef'].nativeElement, 'cdk-focused');
+        this.ren.removeClass(el['_elementRef'].nativeElement, 'cdk-program-focused');
+        this.currentCheckedValue = null;
+      } else {
+        this.currentCheckedValue = el.value
+      }
+    })
   }
 
 }
